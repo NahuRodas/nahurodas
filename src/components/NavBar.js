4ddaed1b187739dbff85d451d0dde634ad936424
NahuRodas <cesarnahuelrodas@gmail.com>
2023-03-02 18:06:33 -0300

    nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
+import "../styles/NavBar.scss"
 
 export class NavBar extends Component {
   render() {
     return (
       <div className='navbar'>
         <div className="container">
-          <div className="navbar-btn">
+          <div className="navbar-btn-menu">
             <button className='btn btn-hide-menu'><i class='bx bx-menu'></i></button>
           </div>
-          <div className='main-navbar'>
+          <div className='menu'>
             <div className="navbar-btn">
             <button className='btn btn-sobre-mi'>Sobre mi</button>
           </div>
@@ -21,9 +22,14 @@ export class NavBar extends Component {
           <div className="navbar-btn">
             <button className='btn btn-contacto'>Contacto</button>
           </div>
+          <div className="dark-mode">
+            <div className="navbar-btn">
+              <button className='btn-toggle-dark-mode'><i class='bx bx-moon'></i></button>
+            </div>
+            <div className="navbar-btn">
+              <button className='btn-toggle-light-mode'><i class='bx bx-sun' ></i></button>
+            </div>
           </div>
-          <div className="navbar-btn">
-            <button className='btn-toggle-dark-mode'></button>
           </div>
         </div>
       </div>
