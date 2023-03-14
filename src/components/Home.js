cfb6e528cffe821aee73ceef676e082d654d4658
NahuRodas <cesarnahuelrodas@gmail.com>
2023-03-14 19:35:09 -0300

    small changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,14 +7,14 @@ export class Home extends Component {
       <div className="contenedor">
         <div className="bg-code-top">
           <h4 className='bg-html'>&lt;html&gt;</h4>
-          <h4 className='bg-body'>&lt;body&gt;</h4>
+          <h4 className='bg-body'>&emsp;&lt;body&gt;</h4>
         </div>
         <div className='main-title'>
           <h1 className='title'>NahuRodas</h1>
         </div>
         <div className="bg-code-bot">
+          <h4 className='bg-body'>&emsp;&lt;/body&gt;</h4>
           <h4 className='bg-html'>&lt;/html&gt;</h4>
-          <h4 className='bg-body'>&lt;/body&gt;</h4>
         </div>
       </div>
     )
