5726b3a3d9c6f027df3ac173cf747544adeff3bb
NahuRodas <cesarnahuelrodas@gmail.com>
2023-03-17 16:58:45 -0300

    avance en 'sobre mi'

--- src/components/SobreMi/SobreMi.js
+++ src/components/SobreMi/SobreMi.js
@@ -1,12 +1,28 @@
 import './SobreMi.scss'
+import ProfilePicture from '../../assets/foto_perfil2.PNG'
 
 const SobreMi = () => {
     return(
-        <div className="container">
-            <h1>TEST</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur 
-               adipisicing elit. Nostrum possimus distinctio 
-               beatae vel unde? Pariatur?</p>
+        <div className="contenedor">
+            <div className="contenedor-sobre-mi">
+                <div className="sobre-mi">
+                    <h2>Hola, </h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Molestias amet laboriosam, quo porro quis fugit omnis 
+                        cumque dignissimos rem quibusdam?</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Molestias amet laboriosam, quo porro quis fugit omnis 
+                        cumque dignissimos rem quibusdam?</p>
+                </div>
+                <div className="profile-picture">
+                    <div className="pp-bg">
+                    
+                </div>
+                <div className="pp-img">
+                    <img src={ ProfilePicture } alt="Foto de perfil" />
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
