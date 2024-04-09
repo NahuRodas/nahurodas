8c22b82f93d9068b4147b3b5b80b6b7b3e2c057c
Nahuel Rodas <cesarnahuelrodas@gmail.com>
2024-04-09 16:11:07 -0300

    Update SobreMi.js

--- src/components/SobreMi/SobreMi.js
+++ src/components/SobreMi/SobreMi.js
@@ -8,7 +8,7 @@ const SobreMi = () => {
                 <div className="sobre-mi">
                     <div className="contenido-sobre-mi">
                         <h2>¡Hola! </h2>
-                        <p>Soy Nahuel, tengo 25 años y resido en Hurlingham, Zona Oeste. Poseo conocimientos en <span>front-end</span> y  
+                        <p>Soy Nahuel, tengo 26 años y resido en Hurlingham, Zona Oeste. Poseo conocimientos en <span>front-end</span> y  
                            <span> back-end</span>, como así tambien en <span>diseño web</span> y <span>diseño gráfico</span>.</p>
                         <p>Graduado de <span>Técnico en informática</span> en la escuela secundaria 
                             Sagrado corazon de Jesús, Hurlingham, Buenos Aires, Argentina. 
